ed4ac66149b90c4c530a8c09cff6f9f56f63d234
KaioGBaldo <kaiobaldo@yahoo.com.br>
2025-04-21 14:32:43 -0300

    Update 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
-function styles(){
-    return gulp.src('.src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+function styles() {
+    return gulp.src('./src/styles/*.scss') // Fixed path
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function(){
-    gulp.watch('.src/styles/*.scss', gulp.parallel(styles));
+function images() {
+    return gulp.src('./src/images/**/*') // Fixed path
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+function watchFiles() {
+    gulp.watch('./src/styles/*.scss', styles); // Fixed path
+}
+
+// Export tasks
+exports.styles = styles;
+exports.images = images;
+exports.watch = watchFiles;
+exports.default = gulp.parallel(styles, images);
