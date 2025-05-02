231c55329aae8a292d8b6cc2bc63da59327bf6f0
KaioGBaldo <kaiobaldo@yahoo.com.br>
2025-05-01 22:36:27 -0300

    Update 1.5

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js') // Fixed path
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // Fixed path
@@ -15,11 +22,12 @@ function images() {
 }
 
 function watchFiles() {
-    gulp.watch('./src/styles/*.scss', styles); // Fixed path
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/styles/*.js', scripts); // Fixed path
 }
 
 // Export tasks
 exports.styles = styles;
 exports.images = images;
 exports.watch = watchFiles;
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts, watchFiles); // Added scripts to default task
